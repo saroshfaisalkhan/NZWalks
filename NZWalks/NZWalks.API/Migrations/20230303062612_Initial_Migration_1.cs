80510c92825a60372ba4b11f13caf7bd77f30301
saroshfaisalkhan@gmail.com <saroshfaisalkhan@gmail.com>
2023-03-02 22:36:06 -0800

    End Of Section 2

---
+++ NZWalks/NZWalks.API/Migrations/20230303062612_Initial_Migration_1.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NZWalks.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial_Migration_1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Lenghth",
+                table: "Walks",
+                newName: "Length");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Length",
+                table: "Walks",
+                newName: "Lenghth");
+        }
+    }
+}
---
+++ NZWalks/NZWalks.API/Models/Domain/Region.cs
@@ -0,0 +1,17 @@
+﻿namespace NZWalks.API.Models.Domain
+{
+    public class Region
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public double Area { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public long Population { get; set; }
+
+        //Navigation Property
+        // It means that one region can have multiple walk
+        public IEnumerable<Walk>?Walks { get; set; }
+    }
+}
